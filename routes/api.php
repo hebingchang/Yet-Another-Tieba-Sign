0db918e15fe77bf0062967fd7893ea2884ef4325
hebingchang <hebingchang1@live.com>
2018-07-31 09:38:59 +0800

    fix a bug

--- routes/api.php
+++ routes/api.php
@@ -21,7 +21,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::group(['middleware' => 'auth:api'], function () {
-    Route::post("/logout", "APiController@logout");
+    Route::post("/logout", "ApiController@logout");
 
     Route::post('/bduss/bind', "ApiController@ApiBindBDUSS");
     Route::get('/bduss/get', "ApiController@ApiGetBDUSS");
